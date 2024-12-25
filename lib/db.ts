316d133ac66bc4b9124e661fc1c2212a6a812238
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2024-12-25 12:42:46 +0530

    db

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
-// Use a global variable to ensure PrismaClient is a singleton in development
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+    var prisma: PrismaClient | undefined;
 }
 
-export const db = globalForPrisma.prisma ?? new PrismaClient()
+export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = db
+if (process.env.NODE_ENV === 'development') {
+  globalThis.prisma = db;
 }
