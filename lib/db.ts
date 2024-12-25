3db4282e9ec9dd42b96cafd91e86c7af910365cc
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2024-12-25 17:54:43 +0530

    neon db

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,6 @@ declare global {
 
 export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
 }
--- prisma/migrations/20241224091810_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Folder" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "parentId" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    CONSTRAINT "Folder_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Folder" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
